ce12440572382014de0a8ff2e0389a7c20b7abc8
loleandote <48246750+loleandote@users.noreply.github.com>
2023-04-14 15:36:38 +0200

    cambio

--- esqueletoSesion2/src/linea.c
+++ esqueletoSesion2/src/linea.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -7,28 +6,36 @@
 #include <memoriaI.h>
 #include <semaforoI.h>
 
-// Modulo principal
 int main(int argc,char *argv[]){
 
 	//TODO: Esquema especificado en la práctica.
 	pid_t pid = getpid();
-	int valorEspera= obtener_var(LLAMADASESPERA);
+    int valorEspera =  obtener_var (LLAMADASESPERA);
+    int i =0;
     // Coge semáforos y memoria compartida
-
+    sem_t *telefono= get_sem(TELEFONOS);
+    sem_t  *mutex = get_sem(MUTEXESPERA);
+    sem_t *linea = get_sem(LINEAS);
+   // No hay llamadas en espera
 
     // Realiza una espera entre 1..60 segundos
     printf("Linea [%d] esperando llamada...\n",pid);
     sleep(rand() % 30 + 1);
-
+    
     //Aumenta las llamadas en espera
-
-
+    wait_sem(mutex);
+    consultar_var(valorEspera, &i);
+    modificar_var(valorEspera,++i);
+    signal_sem(mutex);
+    printf("Linea [%d] esperando telefono libre...Nº Llamadas en espera: %d\n",pid,i);
     // Espera telefono libre
-    printf("Linea [%d] esperando telefono libre...Nº Llamadas en espera: %d\n",pid,valorEspera);
-
+   
+    wait_sem(telefono);
+    
     // Lanza la llamada
     printf("Linea [%d] desviando llamada a un telefono...\n",pid);
 
-
+    signal_sem(linea);
+    
     return EXIT_SUCCESS;
 }
---
+++ esqueletoSesion2/src/telefono.c
@@ -0,0 +1,51 @@
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>   
+
+#include <definitions.h>
+#include <memoriaI.h>
+#include <semaforoI.h>
+
+// Modulo principal
+int main(int argc,char *argv[]){
+    
+
+	//TODO: Esquema según especificado en la práctica.
+    
+    // Define variables locales
+    pid_t pid = getpid();
+    int valorEspera =  obtener_var (LLAMADASESPERA);
+    int i =0;
+    // Coge semáforos y memoria compartida
+    sem_t *telefono= get_sem(TELEFONOS);
+    sem_t  *mutex = get_sem(MUTEXESPERA);
+    sem_t *linea = get_sem(LINEAS);
+    // Se pone en estado de libre incrementando el número de teléfonos libres
+    while(1){
+        // Mensaje de Espera
+        printf("Teléfono [%d] en espera...\n",pid);
+
+		//TODO: Aquí hay que realizar procesos
+        
+        signal_sem(telefono);
+        wait_sem(linea);
+        // Obtenemos el número de llamadas en espera
+        
+        // Mensaje de en conversacion
+        //decrementamos el numero de llamadas en espera
+        wait_sem(mutex);
+        consultar_var(valorEspera, &i);
+        modificar_var(valorEspera,--i);
+       
+        signal_sem(mutex);
+         printf("Teléfono [%d] en conversacion... Nº Llamadas en espera: %d\n",pid,i);
+
+        //volvemos al estado de espera inicial 
+        //signal_sem(LINEAS);
+        // Espera en conversación
+        sleep(rand() % 10 + 10);
+    }
+    return EXIT_SUCCESS;
+}
+ 
